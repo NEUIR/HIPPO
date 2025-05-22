5e54be485a7fa32bf7238eeb2aafeb0dfac857b4
wanghaolan123 <147288584+wanghaolan123@users.noreply.github.com>
2025-05-22 22:30:35 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,22 +1,18 @@
 # dpo training
-export NCCL_P2P_DISABLE=1
-nproc_per_node=2
-CUDA_VISIBLE_DEVICES=0,1 \
-NPROC_PER_NODE=$nproc_per_node \
 swift rlhf \
 --rlhf_type dpo \
 --model #model_path \
 --train_type lora \
 --dataset # dpo_data \
 --torch_dtype bfloat16 \
---num_train_epochs 1 \
+--num_train_epochs #train_epoch \
 --per_device_train_batch_size 1 \
 --per_device_eval_batch_size 1 \
 --learning_rate 1e-4 \
 --lora_rank 8 \
 --lora_alpha 32 \
 --target_modules all-linear \
---gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
+--gradient_accumulation_steps 8 \
 --eval_steps 100 \
 --save_steps 100 \
 --save_total_limit 5 \
